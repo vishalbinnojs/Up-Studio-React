0e603516c3f14ab2b1c008103ddc80a0cd5eff56
Vishal_Bhardwaj <vishalbinnojs@gmail.com>
2026-01-31 06:00:51 +0000

    lazy router imports and added redirects for netlity for react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import { Routes, Route, useLocation } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
+// Eager imports
 import Nav_Links  from "./components/Home/Nav_Links";
 import Home from "./components/Home/Home";
-import View from "./components/Home/View";
-import How_It_Works from "./components/How_It_Works/How_It_Works";
-import Features from "./components/Features/Features";
-import FAQs from "./components/FAQs/FAQs-Contact";
-import Login from "./components/Login/Login";
-import Signup from "./components/Signup/Signup";
-import About from "./components/About_Us/About";
-import Artist from "./components/Profiles/Artist";
-import Technician from "./components/Profiles/Technician";
-import Owner from "./components/Profiles/Owner";
-import PaymentMain from "./components/PaymentGateway/PaymentMain"
 import Footer from "./components/Footer/Footer";
+
+// lazy imports
+const Login = lazy(() => import("./components/Login/Login"));
+const Signup = lazy(() => import("./components/Signup/Signup"));
+const About = lazy(() => import("./components/About_Us/About"));
+const Artist = lazy(() => import("./components/Profiles/Artist"));
+const Technician = lazy(() => import("./components/Profiles/Technician"));
+const Owner = lazy(() => import("./components/Profiles/Owner"));
+const PaymentMain = lazy(() =>
+  import("./components/PaymentGateway/PaymentMain")
+);
+const View = lazy(()=> import('./components/Home/View'))
+const How_It_Works = lazy(() => import('./components/How_It_Works/How_It_Works'))
+const Features = lazy(()=> imports('./components/Features/Features'))
+const FAQs = lazy(()=> imports('./components/FAQs/FAQs-Contact'));
+
 
 import "./App.css";
 
@@ -26,6 +34,7 @@ function App() {
 
   return (
     <>
+    <Suspense fallback={<PageLoader />}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/howitworks" element={<How_It_Works />} />
@@ -41,12 +50,25 @@ function App() {
         <Route path ='/payment' element={<PaymentMain/>} />
        
       </Routes>
+      </Suspense>
           {/* Hide NavLinks */}
       {!hideNavLinks && <Nav_Links />}   {/* This is short circuiting */}
           {/* Hide Footer */}
       {!shouldHideFooter && <Footer />}
     </>
   );
+}
+const PageLoader = () => {
+  
+  return (
+    <>
+  <div className="pageLoader">
+      <div className="pulse-ring"></div>
+      <div className="pulse-ring delay"></div>
+      <div className="music-text">Loading the vibe… 🎵</div>
+    </div>
+    </>
+  )
 }
 
 export default App;
